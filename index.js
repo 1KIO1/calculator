9e680ae05a4fec011245396fd3f4ee2fd4ae5dbd
GAB <162764086+1KIO1@users.noreply.github.com>
2024-03-09 09:40:22 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById("display");
+
+function appendTodisplay(input){
+    display.value += input;
+}
+
+function cleardisplay (){
+    display.value = "";
+}
+
+function calculate(){
+   try{
+    display.value = eval(display.value);
+   }
+  catch(error){
+    display.value ="error";
+  }
+}
+
